3a9887ab4747d9bf67fd219e39d7d49bc10ad9cd
Vimeow <vy.nguyen.professional@gmail.com>
2023-12-03 17:33:56 +1030

    Ready for sunmission

--- DataAnalysis.sql
+++ DataAnalysis.sql
@@ -6,7 +6,7 @@ ON e.emp_no = s.emp_no
 ORDER BY e.emp_no ASC;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
-SELECT emp_no, last_name, first_name, sex, hire_date
+SELECT emp_no, last_name, first_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01'AND '1986-12-31'
 ORDER BY emp_no ASC;
